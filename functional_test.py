f7b7381949841137da6bc4f5c98ab9ba19cacb2e
xieziwei99 <xieziwei@bupt.edu.cn>
2020-04-16 18:34:27 +0800

    新增输入两条 to-do list 的功能测内容

--- functional_test.py
+++ functional_test.py
@@ -36,21 +36,30 @@ class NewVisitorTest(unittest.TestCase):     # 测试类以Test结尾
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows),
-                        "New to-do item did not appear in table")
+        # self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows),
+        #                 f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        self.assertIn('1: Buy peacock features', [row.text for row in rows])
 
         # 这里还有一个输入框让他输入另外一个项，他输入"Use peacock features to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock features to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 页面又更新了，显示出两条item
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock features', [row.text for row in rows])
+        self.assertIn('2: Use peacock features to make a fly', [row.text for row in rows])
+
+        # 张三想知道网站是否记住了他的代办列表，他发现网站为他生成了一个独立的URL
+
+        # 他再次访问了那个URL，发现to-do list还在
+
+        # 他很满意的睡觉去了
+        
         self.fail('Finish the test.')
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-# 页面又更新了，显示出两条item
-
-# 张三想知道网站是否记住了他的代办列表，他发现网站为他生成了一个独立的URL
-
-# 他再次访问了那个URL，发现to-do list还在
-
-# 他很满意的睡觉去了
--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,6 @@ from django.http import HttpRequest, HttpResponse
 
 
 def home_page(request: HttpRequest):
-    return render(request, 'home.html')
+    # request.POST.get('item_text', '')
+    # 如果是以 get 方法请求，则没有传递 item_text 作为表单数据，用此方法返回空字符串，不至于报错KeyError
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
