de73c816d0532bb996b830dd9dd873d6cb365c8b
xieziwei99 <xieziwei@bupt.edu.cn>
2020-04-16 18:45:13 +0800

    重构，新增函数check_for_row_in_list_table

--- functional_test.py
+++ functional_test.py
@@ -34,11 +34,12 @@ class NewVisitorTest(unittest.TestCase):     # 测试类以Test结尾
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows),
-        #                 f"New to-do item did not appear in table. Contents were:\n{table.text}")
-        self.assertIn('1: Buy peacock features', [row.text for row in rows])
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # # self.assertTrue(any(row.text == '1: Buy peacock features' for row in rows),
+        # #                 f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        # self.assertIn('1: Buy peacock features', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock features')
 
         # 这里还有一个输入框让他输入另外一个项，他输入"Use peacock features to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -47,18 +48,22 @@ class NewVisitorTest(unittest.TestCase):     # 测试类以Test结尾
         time.sleep(1)
 
         # 页面又更新了，显示出两条item
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock features', [row.text for row in rows])
-        self.assertIn('2: Use peacock features to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock features')
+        self.check_for_row_in_list_table(
+            '2: Use peacock features to make a fly')
 
         # 张三想知道网站是否记住了他的代办列表，他发现网站为他生成了一个独立的URL
 
         # 他再次访问了那个URL，发现to-do list还在
 
         # 他很满意的睡觉去了
-        
+
         self.fail('Finish the test.')
+
+    def check_for_row_in_list_table(self, row_text: str):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 
 if __name__ == "__main__":
