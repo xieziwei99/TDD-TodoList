a2ca418e9964821f915c80c7e8a7e040b232326f
xieziwei99 <xieziwei@bupt.edu.cn>
2020-04-28 11:06:30 +0800

    POST后重定向 & 在模版页中显示所有items

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request: HttpRequest):
-    # request.POST.get('item_text', '')
-    # 如果是以 get 方法请求，则没有传递 item_text 作为表单数据，用此方法返回空字符串，不至于报错KeyError
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+    # # request.POST.get('item_text', '')
+    # # 如果是以 get 方法请求，则没有传递 item_text 作为表单数据，用此方法返回空字符串，不至于报错KeyError
+    # return render(request, 'home.html', {'new_item_text': item.text})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
